2bdecc03b98a84e8483d7a1dd0beebc28c498b14
mohamedreda3 <mmoh33653@gmail.com>
2023-02-22 22:46:22 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ menu.onclick = () => {
 window.onscroll = () => {
     document.documentElement.scrollTop > document.querySelector('header').offsetHeight ? document.querySelector('nav').classList.add('sticky') : document.querySelector('nav').classList.remove('sticky')
 }
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq, index) => {
+    faq.onclick = (e) => {
+        faqs.forEach((faq_1, i) => index != i ? faq_1.classList.remove('active') : null);
+        e.currentTarget.classList.contains('active')
+            ? e.currentTarget.classList.remove('active')
+            : e.currentTarget.classList.add('active')
+    }
+})
