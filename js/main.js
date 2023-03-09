7bc7a08f546375d4b324759595d6f340f539b3d5
mohamedreda3 <mmoh33653@gmail.com>
2023-03-09 22:59:50 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-var menu = document.querySelector('.menu');
-menu.onclick = () => {
+
+window.onscroll = () => {
+    document.documentElement.scrollTop > document.querySelector('header').offsetHeight + 10 ?
+        document.querySelector('nav').classList.add('sticky')
+        : document.querySelector('nav').classList.remove('sticky');
+}
+
+document.querySelector('.menu').onclick = () => {
     document.querySelector('nav ul').classList.toggle('active');
 }
 
-window.onscroll = () => {
-    document.documentElement.scrollTop > document.querySelector('header').offsetHeight ? document.querySelector('nav').classList.add('sticky') : document.querySelector('nav').classList.remove('sticky')
-}
 
-const faqs = document.querySelectorAll('.faq');
+var questionBoxess = document.querySelectorAll('.question-box');
 
-faqs.forEach((faq, index) => {
-    faq.onclick = (e) => {
-        faqs.forEach((faq_1, i) => index != i ? faq_1.classList.remove('active') : null);
-        e.currentTarget.classList.contains('active')
-            ? e.currentTarget.classList.remove('active')
-            : e.currentTarget.classList.add('active')
+questionBoxess.forEach((box, index) => {
+    box.onclick = (e) => {
+        questionBoxess.forEach((box, _i) => index == _i ? null : questionBoxess[_i].classList.remove('active'))
+        e.currentTarget.classList.toggle('active');
     }
 })
+
